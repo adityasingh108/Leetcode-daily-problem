13b6be417e8034769a72ded4394d5d36794b28f2
adityasingh108 <adityasingh78085@gmail.com>
2022-03-19 20:38:33 +0530

    18 and 19 march

---
+++ 895. Maximum Frequency Stack.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+class FreqStack {
+public:
+    priority_queue<vector<int>> pq; 
+    unordered_map<int, int> m; 
+    int count=0; 
+    public:
+    FreqStack(){ 
+    }
+    void push(int x){
+        ++m[x];
+        pq.push({m[x],count++,x}); 
+    }
+    int pop(){
+        auto maxx = pq.top();
+        pq.pop();
+        --m[maxx[2]];
+        return maxx[2]; 
+    }
+};
