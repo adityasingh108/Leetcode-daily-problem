ebb2e67bee7eed2250b7a8801fbbe014455fa4d4
adityasingh108 <adityasingh78085@gmail.com>
2022-03-22 11:16:28 +0530

    22 march

---
+++ 1663. Smallest String With A Given Numeric Value.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+string getSmallestString(int n , int k){
+    string res(n,'a');
+    k-= n;
+    while(k>0){
+        res[--n] += min(25, k);
+        k-=min(25,k);
+    }
+    return res;
+}
